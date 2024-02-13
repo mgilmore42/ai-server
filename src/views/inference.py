b44637307c79264e0eb8be29aba4b798ea6973ee
Mitchell Gilmore <mgilm0re@bu.edu>
2024-02-12 23:02:28 -0500

    added inference API

--- src/views/inference.py
+++ src/views/inference.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, current_app, jsonify, request
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +11,45 @@ inference_blueprint = Blueprint('inference', __name__)
 
 @inference_blueprint.route('/infer', methods=['POST'])
 def perform_inference() -> Response:
-	"""TBD."""  # TODO: Add description
-	logger.info('Inference performed.')
-	return jsonify({'message': 'Inference performed.', 'result': 'dummy_result'})
+	"""Performs inference.
+
+	Parameters
+	----------
+	name : str
+		The name of the model to perform inference with.
+	version : str
+		The version of the model to perform inference with.
+	data : object
+		The data to perform inference on.
+
+	Returns
+	-------
+	Response
+		The response containing the inference information.
+
+	"""
+	return jsonify(process_inference(**request.json))
+
+
+def process_inference(name: str, user: str, version: str, data: object) -> dict:
+	"""Processes inference request.
+
+	Parameters
+	----------
+	name : str
+		The name of the model to perform inference with.
+	user : str
+		The user requesting the inference.
+	version : str
+		The version of the model to perform inference with.
+	data : object
+		The data to perform inference on.
+
+	Returns
+	-------
+	dict
+		The response containing the inference information.
+
+	"""
+	current_app.logger.info(f'User: {user} requested inference for model {name}.')
+	return {'model': name, 'version': version, 'user': user, 'data': data}
