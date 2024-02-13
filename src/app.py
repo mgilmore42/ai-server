2348f6caf78692e2213d4acb2ebb976b0116d421
Mitchell Gilmore <mgilm0re@bu.edu>
2024-02-12 23:32:11 -0500

    fixed info rename

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@
 from config import Config
 from flask import Flask
 from views.inference import inference_blueprint
-from views.status import status_blueprint
+from views.info import info_blueprint
 from views.training import training_blueprint
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ app.config.from_object(Config)
 # Register Blueprints
 app.register_blueprint(training_blueprint, url_prefix='/api')
 app.register_blueprint(inference_blueprint, url_prefix='/api')
-app.register_blueprint(status_blueprint, url_prefix='/api')
+app.register_blueprint(info_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
 	app.run()
