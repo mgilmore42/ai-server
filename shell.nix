0be3a013e690fd0ef7709084bd757b812cc3e719
Mitchell Gilmore <mgilm0re@bu.edu>
2024-03-21 09:13:23 -0400

    added queue dependencies to nix

--- shell.nix
+++ shell.nix
@@ -4,7 +4,10 @@ in pkgs.mkShell {
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.flask
+      python-pkgs.celery
+      python-pkgs.amqp
     ]))
     pkgs.ruff
+    pkgs.python3
   ];
 }
