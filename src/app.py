d312f319bf00f309ce419a52d42b9ef88cf174a1
Mitchell Gilmore <mgilm0re@bu.edu>
2024-05-05 11:25:54 -0400

    added basic poetry support

--- src/app.py
+++ src/app.py
@@ -1,18 +1,21 @@
 """Main application file for the Flask API."""
 
-from config import Config
+from .config import Config
 from flask import Flask
-from views.inference import inference_blueprint
-from views.info import info_blueprint
-from views.training import training_blueprint
+from .views.inference import inference_blueprint
+from .views.info import info_blueprint
+from .views.training import training_blueprint
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def run():
+	app = Flask(__name__)
+	app.config.from_object(Config)
 
-# Register Blueprints
-app.register_blueprint(training_blueprint, url_prefix='/api')
-app.register_blueprint(inference_blueprint, url_prefix='/api')
-app.register_blueprint(info_blueprint, url_prefix='/api')
+	# Register Blueprints
+	app.register_blueprint(training_blueprint, url_prefix='/api')
+	app.register_blueprint(inference_blueprint, url_prefix='/api')
+	app.register_blueprint(info_blueprint, url_prefix='/api')
+
+	app.run()
 
 if __name__ == '__main__':
-	app.run()
+	run()
